ecd1b1966e9825ca50cfd926cee43ae552ad7878
Andrey Shevchuk <campugnatus@gmail.com>
2021-04-03 02:25:37 +0300

    well, that's 1.0

--- contentScript.js
+++ contentScript.js
@@ -6,7 +6,7 @@ var globalObserver = new MutationObserver(function globalMutationCallback (mutat
         let article = articles[i];
         if (article.getAttribute("role") === 'presentation') {
             // don't be synchronous in this callback
-            setTimeout(() => handleArticle(article), 100);
+            setTimeout(() => handleArticle(article), 128);
         }
     }
 });
@@ -34,6 +34,7 @@ function addButton (img) {
         return;
 
     let btn = document.createElement("button");
+    btn.setAttribute("title", "Open image in new tab");
     btn.className = 'blow-up-button';
     btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
